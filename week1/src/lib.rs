94367a04bcb35df56c93fcd564f57679dba3f5a4
v1xingyue <qixingyue@gmail.com>
2023-06-19 17:48:06 +0800

    export some fun

--- week1/src/lib.rs
+++ week1/src/lib.rs
@@ -1,4 +1,4 @@
 mod ed25519;
 mod utils;
-pub use ed25519::{get_keypair, msg_hash, to_address};
+pub use ed25519::{get_keypair, msg_hash, sui_sign_data_example, sui_signer_validator, to_address};
 pub use utils::{current_timestamp, mark_line};
